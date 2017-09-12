05831da1959ec1403565367835978ca6504a2ea5
Garrick Kwong <gkwong@antioch.edu>
2017-09-12 09:50:08 -0700

    Added comments for EmailService

--- Email/EmailService.cs
+++ Email/EmailService.cs
@@ -1,7 +1,9 @@
 namespace EmailServices
 {
     public class EmailService : IEmailService
-    {
+    {   
+        // method that is called and takes parameters to prepare to send off.  .wait() is required here because procedure
+        // is run async, wait() will sync
         public void Send(IEnumerable<MailAddress> to, MailAddress from, string subject, string htmlContent, string htmlBody)
         {
             Execute(to, from, subject, htmlContent, htmlBody).Wait();
@@ -18,10 +20,13 @@ namespace EmailServices
                 HtmlContent = htmlContent
             };
 
+            // loop through to, which can be any type of collection that holds email addresses
             foreach(var i in to)
             {
                 msg.AddTo(new EmailAddress(i.Address, i.DisplayName));
             }
+
+            // ConfigureAwait(false) is for fixing unnecessary thread context synchronization and blocks that will occur when running due to async/await
             var response = await client.SendEmailAsync(msg).ConfigureAwait(false);
 
             if ((int)response.StatusCode >= 300)
